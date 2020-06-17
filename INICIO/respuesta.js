46625ee086b022f27ee9e6c8b84f83336705e52c
mateomaciel <63106091+mateomaciel@users.noreply.github.com>
2020-06-16 23:34:10 -0300

    mejorado

--- INICIO/respuesta.js
+++ INICIO/respuesta.js
@@ -1,17 +1,30 @@
-console.log("terminon de funcionar")
-
 let queryString = window.location.search
-
 let objetoQuery = new URLSearchParams(queryString);
-
 var resultado = objetoQuery.get('busqueda');
-
-console.log(resultado)
 
 fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=" + resultado)
 .then(function(response){
     return response.json()
 })
-.then(function(data){
-console.log(data)
+
+.then(function(dato){
+
+let answer = dato.data;
+console.log (answer)
+
+let tracks = document.querySelector('#resultados')
+
+answer.forEach(function(dataCancion){
+    tracks.innerHTML += `<div>
+    <iframe id="track" scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist
+    =true&width=700&height=350&color=ff0000&layout=dark&size=medium&type=tracks&id=` + dataCancion.id +`&app_id=1"></iframe></div>`
+
+if (answer == "null") {
+//ak falta agregar que cuando sea null no se muestre nada
+
+
+
+}
+
+})
 })
