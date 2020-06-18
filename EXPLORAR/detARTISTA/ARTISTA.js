7770c6aa87fc26f0059da4bd4fa16bfe041e9483
mateomaciel <63106091+mateomaciel@users.noreply.github.com>
2020-06-18 18:02:43 -0300

    lo de mateo esta terminado

--- EXPLORAR/detARTISTA/ARTISTA.js
+++ EXPLORAR/detARTISTA/ARTISTA.js
@@ -40,7 +40,7 @@ fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/' + art
         let cancions = document.querySelector('#topfives');
        
         song.forEach(function(dataArtist){
-            cancion.innerHTML += `<div id="widget"> <iframe id="frame"; scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=400&height=350&color=007FEB&layout=dark&size=medium&type=tracks&id=${dataArtist.id}&app_id=1" width="400" height="90"></iframe>
+            cancion.innerHTML += `<div id="widgets"> <iframe id="frames"; scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=400&height=350&color=007FEB&layout=dark&size=medium&type=tracks&id=${dataArtist.id}&app_id=1" width="400" height="90"></iframe>
             <div id="boton"><a href="/EXPLORAR/detCANCION/CANCION.html?id=${dataArtist.id}"  style="text-decoration: none;color:black"><p id="mas">VER MAS</p></a></div></div>`  
             
             
